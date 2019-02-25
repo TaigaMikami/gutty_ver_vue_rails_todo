cbdeb08aae099baa0c09be6b4f28da04a37c04fc
三上大河 <mikamimikami60@gmail.com>
2019-02-25 19:04:01 +0900

    不要な部分を削除1

--- frontend/src/components/Task.vue
+++ frontend/src/components/Task.vue
@@ -18,66 +18,9 @@
   import ReadyTask from '@/components/modules/ReadyTask'
   import DoingTask from '@/components/modules/DoingTask'
   import Form from '@/components/modules/Form'
-  import { mapGetters } from 'vuex'
 
   export default {
     name: 'Task',
-    data: () => ({
-      pagination: {
-        sortBy: 'title'
-      },
-      selected: [],
-      ready_tasks: [],
-    }),
-    computed: mapGetters({
-      headers: 'getHeaders'
-    }),
-    mounted() {
-      this.fetchTask();
-    },
-
-    methods: {
-      toggleAll () {
-        if (this.selected.length) this.selected = [];
-        else {
-          this.selected = this.tasks.slice()
-          for(let id in this.selected) {
-            this.doneTask(id);
-          }
-        }
-
-      },
-      changeSort (column) {
-        if (this.pagination.sortBy === column) {
-          this.pagination.descending = !this.pagination.descending
-        } else {
-          this.pagination.sortBy = column;
-          this.pagination.descending = false
-        }
-      },
-      fetchTask () {
-        this.$axios.get('http://localhost:3000/tasks')
-          .then(response => {
-            this.ready_tasks = response.data;
-            console.log(response.data);
-          })
-          .catch((reason) => {
-            console.log(reason);
-          });
-      },
-
-      doneTask (task_id) {
-        this.$axios.put('http://localhost:3000/tasks/' + task_id, { task: { status: 2 }})
-          .then(response => {
-            console.log(response);
-            location.reload();
-          })
-          .catch((reason) => {
-            console.log(reason);
-          });
-      }
-    },
-
     components: {
       ReadyTask,
       DoingTask,
